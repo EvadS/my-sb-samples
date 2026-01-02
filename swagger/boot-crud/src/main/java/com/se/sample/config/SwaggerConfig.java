d1107f16cff394f80a516b12fb8ee2eebaf8ac36
Скиба Євген <yevhen.skyba@ligazakon.ua>
2026-01-02 14:16:52 +0200

    swagger baase auth, version to swagger definition

--- swagger/boot-crud/src/main/java/com/se/sample/config/SwaggerConfig.java
+++ swagger/boot-crud/src/main/java/com/se/sample/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.se.sample.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +14,21 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
+
+    @Value("${spring.application.version}")
+    private String buildVersion;
+
+    @Value("${spring.application.description}")
+    private String sescription;
+
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("JavaInUse Authentication Service"))
+                .info(new Info().title("JavaInUse Authentication Service")
+                        .description(sescription)
+                        .version(buildVersion)
+                )
                 .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
                 .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
                         .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("basic")));
